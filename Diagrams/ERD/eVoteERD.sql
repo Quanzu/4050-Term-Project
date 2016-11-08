94ace4e1089ee1141b282d74be5999dde3e591e6
Phi Long <philong.nguyen95@gmail.com>
2016-11-07 22:20:48 -0500

    added initial sql

---
+++ Diagrams/ERD/eVoteERD.sql
@@ -0,0 +1,174 @@
+CREATE TABLE Ballot (
+ ballotId INT NOT NULL AUTO_INCREMENT,
+ openDate DATE,
+ closeDate DATE
+);
+
+ALTER TABLE Ballot ADD CONSTRAINT PK_Ballot PRIMARY KEY (ballotId);
+
+
+CREATE TABLE Candidate (
+ candidateId INT NOT NULL AUTO_INCREMENT,
+ name CHAR(20),
+ voteCount INT,
+ isAlternate INT
+);
+
+ALTER TABLE Candidate ADD CONSTRAINT PK_Candidate PRIMARY KEY (candidateId);
+
+
+CREATE TABLE Election (
+ electionId INT NOT NULL AUTO_INCREMENT,
+ office CHAR(15),
+ isPartisan INT,
+ alternateAllowed INT,
+ voteCount CHAR(10)
+);
+
+ALTER TABLE Election ADD CONSTRAINT PK_Election PRIMARY KEY (electionId);
+
+
+CREATE TABLE ElectionBallot (
+ electionId INT NOT NULL,
+ ballotId INT NOT NULL
+);
+
+ALTER TABLE ElectionBallot ADD CONSTRAINT PK_ElectionBallot PRIMARY KEY (electionId,ballotId);
+
+
+CREATE TABLE ElectoralDistrict (
+ districtId INT NOT NULL AUTO_INCREMENT,
+ districtName CHAR(15)
+);
+
+ALTER TABLE ElectoralDistrict ADD CONSTRAINT PK_ElectoralDistrict PRIMARY KEY (districtId);
+
+
+CREATE TABLE Issue (
+ issueId INT NOT NULL AUTO_INCREMENT,
+ question CHAR(100),
+ yesCount INT,
+ noCount INT
+);
+
+ALTER TABLE Issue ADD CONSTRAINT PK_Issue PRIMARY KEY (issueId);
+
+
+CREATE TABLE IssueBallot (
+ issueId INT NOT NULL,
+ ballotId INT NOT NULL
+);
+
+ALTER TABLE IssueBallot ADD CONSTRAINT PK_IssueBallot PRIMARY KEY (issueId,ballotId);
+
+
+CREATE TABLE Party (
+ partyId INT NOT NULL AUTO_INCREMENT,
+ partyName CHAR(20)
+);
+
+ALTER TABLE Party ADD CONSTRAINT PK_Party PRIMARY KEY (partyId);
+
+
+CREATE TABLE User (
+ userId INT NOT NULL AUTO_INCREMENT,
+ fname CHAR(20),
+ lname CHAR(20),
+ userName CHAR(20),
+ password CHAR(50),
+ email CHAR(25),
+ address CHAR(50)
+);
+
+ALTER TABLE User ADD CONSTRAINT PK_User PRIMARY KEY (userId);
+
+
+CREATE TABLE Voter (
+ voterId INT NOT NULL AUTO_INCREMENT,
+ userId INT NOT NULL,
+ age INT
+);
+
+ALTER TABLE Voter ADD CONSTRAINT PK_Voter PRIMARY KEY (voterId);
+
+
+CREATE TABLE VoterDistrict (
+ districtId INT NOT NULL,
+ voterId INT NOT NULL
+);
+
+ALTER TABLE VoterDistrict ADD CONSTRAINT PK_VoterDistrict PRIMARY KEY (districtId,voterId);
+
+
+CREATE TABLE VoteRecord (
+ date TIMESTAMP(10) NOT NULL,
+ ballotId INT NOT NULL,
+ voterId INT NOT NULL
+);
+
+ALTER TABLE VoteRecord ADD CONSTRAINT PK_VoteRecord PRIMARY KEY (date,ballotId,voterId);
+
+
+CREATE TABLE BallotDistrict (
+ districtId INT NOT NULL,
+ ballotId INT NOT NULL
+);
+
+ALTER TABLE BallotDistrict ADD CONSTRAINT PK_BallotDistrict PRIMARY KEY (districtId,ballotId);
+
+
+CREATE TABLE CandidateElection (
+ electionId INT NOT NULL,
+ candidateId INT NOT NULL
+);
+
+ALTER TABLE CandidateElection ADD CONSTRAINT PK_CandidateElection PRIMARY KEY (electionId,candidateId);
+
+
+CREATE TABLE CandidateParty (
+ candidateId INT NOT NULL,
+ partyId INT NOT NULL
+);
+
+ALTER TABLE CandidateParty ADD CONSTRAINT PK_CandidateParty PRIMARY KEY (candidateId,partyId);
+
+
+CREATE TABLE ElectionsOfficer (
+ userId INT NOT NULL
+);
+
+ALTER TABLE ElectionsOfficer ADD CONSTRAINT PK_ElectionsOfficer PRIMARY KEY (userId);
+
+
+ALTER TABLE ElectionBallot ADD CONSTRAINT FK_ElectionBallot_0 FOREIGN KEY (electionId) REFERENCES Election (electionId);
+ALTER TABLE ElectionBallot ADD CONSTRAINT FK_ElectionBallot_1 FOREIGN KEY (ballotId) REFERENCES Ballot (ballotId);
+
+
+ALTER TABLE IssueBallot ADD CONSTRAINT FK_IssueBallot_0 FOREIGN KEY (issueId) REFERENCES Issue (issueId);
+ALTER TABLE IssueBallot ADD CONSTRAINT FK_IssueBallot_1 FOREIGN KEY (ballotId) REFERENCES Ballot (ballotId);
+
+
+ALTER TABLE Voter ADD CONSTRAINT FK_Voter_0 FOREIGN KEY (userId) REFERENCES User (userId);
+
+
+ALTER TABLE VoterDistrict ADD CONSTRAINT FK_VoterDistrict_0 FOREIGN KEY (districtId) REFERENCES ElectoralDistrict (districtId);
+ALTER TABLE VoterDistrict ADD CONSTRAINT FK_VoterDistrict_1 FOREIGN KEY (voterId) REFERENCES Voter (voterId);
+
+
+ALTER TABLE VoteRecord ADD CONSTRAINT FK_VoteRecord_0 FOREIGN KEY (ballotId) REFERENCES Ballot (ballotId);
+ALTER TABLE VoteRecord ADD CONSTRAINT FK_VoteRecord_1 FOREIGN KEY (voterId) REFERENCES Voter (voterId);
+
+
+ALTER TABLE BallotDistrict ADD CONSTRAINT FK_BallotDistrict_0 FOREIGN KEY (districtId) REFERENCES ElectoralDistrict (districtId);
+ALTER TABLE BallotDistrict ADD CONSTRAINT FK_BallotDistrict_1 FOREIGN KEY (ballotId) REFERENCES Ballot (ballotId);
+
+
+ALTER TABLE CandidateElection ADD CONSTRAINT FK_CandidateElection_0 FOREIGN KEY (electionId) REFERENCES Election (electionId);
+ALTER TABLE CandidateElection ADD CONSTRAINT FK_CandidateElection_1 FOREIGN KEY (candidateId) REFERENCES Candidate (candidateId);
+
+
+ALTER TABLE CandidateParty ADD CONSTRAINT FK_CandidateParty_0 FOREIGN KEY (candidateId) REFERENCES Candidate (candidateId);
+ALTER TABLE CandidateParty ADD CONSTRAINT FK_CandidateParty_1 FOREIGN KEY (partyId) REFERENCES Party (partyId);
+
+
+ALTER TABLE ElectionsOfficer ADD CONSTRAINT FK_ElectionsOfficer_0 FOREIGN KEY (userId) REFERENCES User (userId);
