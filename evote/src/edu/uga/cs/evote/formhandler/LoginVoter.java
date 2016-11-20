bee67237eb788c45ac3e35fc543b46cd9143f5bb
donovan521 <donovan521@users.noreply.github.com>
2016-11-19 20:28:56 -0500

    Update LoginVoter.java

--- evote/src/edu/uga/cs/evote/formhandler/LoginVoter.java
+++ evote/src/edu/uga/cs/evote/formhandler/LoginVoter.java
@@ -1,11 +1,20 @@
 package edu.uga.cs.evote.formhandler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.uga.cs.evote.EVException;
+import edu.uga.cs.evote.entity.Voter;
+import edu.uga.cs.evote.entity.impl.VoterImpl;
+import edu.uga.cs.evote.logic.VoterController;
 
 /**
  * Servlet implementation class LoginVoter
@@ -26,14 +35,45 @@ public class LoginVoter extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+    	PrintWriter out = response.getWriter();
+    	try{
+    		VoterImpl vo = new VoterImpl();
+    		vo.setUserName(request.getParameter("uname"));
+    		vo.setPassword(request.getParameter("pword"));
+    		//Necessary for Voter?
+    		String option = request.getParameter("option");
+    		
+    		
+    		//calling method login in VO controller?
+    		vo = VoterController.login(vo);
+    		
+    		if (vo.isPersistent())
+    		{
+    			HttpSession session = request.getSession(true);
+    			session.setAttribute("currentSessionUser", vo);
+    			//What should the voter be sent to?
+    			response.sendRedirect("voHomepage.jsp");
+    		}
+    		else
+    			//create invalid login page.
+    			response.sendRedirect("invalidLogin.jsp");
+    		
+    		
+    	} catch (EVException e) {
+			e.printStackTrace();
+		} finally
+    	{
+    		out.close();
+    	}
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
