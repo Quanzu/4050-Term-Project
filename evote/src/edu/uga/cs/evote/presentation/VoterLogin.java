d647fa7569ffe90ea80eee47526b1323cc658974
Phi Long <philong.nguyen95@gmail.com>
2016-11-20 15:16:23 -0500

    restored donovan's voter login

--- evote/src/edu/uga/cs/evote/presentation/VoterLogin.java
+++ evote/src/edu/uga/cs/evote/presentation/VoterLogin.java
@@ -1,9 +1,7 @@
 package edu.uga.cs.evote.presentation;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,52 +9,81 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.uga.cs.evote.EVException;
-import edu.uga.cs.evote.entity.Voter;
-import edu.uga.cs.evote.entity.impl.VoterImpl;
-import edu.uga.cs.evote.logic.impl.VoterRegCtrl;
+
+import edu.uga.cs.evote.logic.LogicLayer;
+import edu.uga.cs.evote.session.Session;
+import edu.uga.cs.evote.session.SessionManager;
+
+
 
 /**
- * Servlet implementation class LoginVoter
+ * Servlet implementation class Login
  */
 @WebServlet("/VoterLogin")
 public class VoterLogin extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-       
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-    	PrintWriter out = response.getWriter();
-    	try{
-    		VoterImpl vo = new VoterImpl();
-    		vo.setUserName(request.getParameter("uname"));
-    		vo.setPassword(request.getParameter("pword"));
     		
-    		//calling method login in VO controller?
-    		vo = VoterRegCtrl.login(vo);
+    	HttpSession httpSession = null;
+    	String username;
+    	String password;
+   		String ssid = null;
+   		String ssid2 = null;
+   		Session session = null;
+   		LogicLayer logicLayer = null;
     		
-    		if (vo.isPersistent())
-    		{
-    			HttpSession session = request.getSession(true);
-    			session.setAttribute("currentSessionUser", vo);
-    			//What should the voter be sent to?
-    			response.sendRedirect("voHomepage.jsp");
-    		}
-    		else
-    			//create invalid login page.
-    			response.sendRedirect("invalidLogin.jsp");
-    		
-    		
-    	} catch (EVException e) {
-			e.printStackTrace();
-		} finally
-    	{
-    		out.close();
-    	}
-		
+    	httpSession = request.getSession();
+    	ssid = (String)httpSession.getAttribute("ssid");
+        if( ssid != null ) {
+        	System.out.println( "Already have ssid: " + ssid );
+            session = SessionManager.getSessionById( ssid );
+            //System.out.println( "Connection: " + session.getConnection() );
+        }
+        else
+            System.out.println( "ssid is null" );
+          
+            
+        if( session == null ) {
+        	try {
+        		session = SessionManager.createSession();
+            }
+            catch ( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+            
+        logicLayer = session.getLogicLayer();
+            
+        username = request.getParameter( "username" );
+        password = request.getParameter( "password" );
+            
+        if( username == null || password == null ) {
+        	System.out.println("Username or password null");
+        	return;
+        }
+            
+        try {          
+            ssid2 = logicLayer.voterLogin( session, username, password );
+            System.out.println( "Obtained ssid: " + ssid2 );
+            httpSession.setAttribute( "ssid", ssid2 );
+            System.out.println( "Connection: " + session.getConnection() );
+        } 
+        catch ( Exception e ) {
+        	e.printStackTrace();
+        }
+            
+            
+        if (ssid2!=null)
+        	response.sendRedirect("voterHomepage.jsp");
+    	else
+    		response.sendRedirect("invalidLogin.jsp");
+    
 	}
 
 }
