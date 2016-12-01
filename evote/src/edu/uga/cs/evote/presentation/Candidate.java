6844113aa0ad2ded76fe1d7721433f04631de45c
Phi Long <philong.nguyen95@gmail.com>
2016-11-30 22:27:47 -0500

    deleted unneccessary candidate.java lines

--- evote/src/edu/uga/cs/evote/presentation/Candidate.java
+++ evote/src/edu/uga/cs/evote/presentation/Candidate.java
@@ -20,21 +20,6 @@ import edu.uga.cs.evote.session.SessionManager;
 public class Candidate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Candidate() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
